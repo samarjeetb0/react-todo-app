b647b4e2168a64dd28ad504ebd3d1730f9248935
samar <samarjeetb0@gmail.com>
2024-02-12 15:08:21 +0530

    Basic way of using reducer

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,54 @@ import { AddTodoUsingUseRef } from "./components/AddTodoUsingUseRef";
 import { AppName } from "./components/AppName";
 import "./App.css";
 import { TodoItems } from "./components/TodoItems";
-import { useState } from "react";
+import { useReducer } from "react";
 import { WelcomeMessage } from "./components/WelcomeMessage";
 import { TodoItemsFromContext } from "./store/todo-items-store";
 
-function App() {
-  const [itemList, setItemList] = useState([]);
-  /**
-   * By default react always works in async way ,
-   * to always get the update current value we are
-   * passing data inside the setTimesList method by using callback
-   *
-   * also call as functional updates to avoid stale values during async updates
-   */
-  const addTodoItem = (todoName, todoDate) => {
-    setItemList((currValue) => [
-      ...currValue,
+const todoItemsReducer = (currentItems, action) => {
+  let newItemsList;
+  if (action.type === "NEW_ITEM") {
+    newItemsList = [
+      ...currentItems,
       {
-        todoName: todoName,
-        todoDate: todoDate,
+        todoName: action.payload.todoName,
+        todoDate: action.payload.todoDate,
       },
-    ]);
+    ];
+  }
+  if (action.type === "DELETE_ITEM") {
+    newItemsList = currentItems.filter(
+      (item) => item.todoName !== action.payload.todoName
+    );
+  }
+  return newItemsList;
+};
+
+function App() {
+  const [itemList, dipatchTodoItemActions] = useReducer(todoItemsReducer, []);
+
+  const addTodoItem = (todoName, todoDate) => {
+    const actionObject = {
+      type: "NEW_ITEM",
+      payload: {
+        todoName,
+        todoDate,
+      },
+    };
+    dipatchTodoItemActions(actionObject);
   };
 
   const deletTodoItem = (todoName) => {
-    let newList = itemList.filter((item) => item.todoName !== todoName);
-    setItemList(newList);
+    const actionObject = {
+      type: "DELETE_ITEM",
+      payload: {
+        todoName
+      },
+    };
+    dipatchTodoItemActions(actionObject);
   };
 
   return (
-    //value should be pass as an bject in the context provider so that later can be modified according to our need
-    // <TodoItemsFromContext.Provider
-    //   value={{
-    //     todoitems: itemList,
-    //     addTodoItem: addTodoItem,
-    //     deletTodoItem: deletTodoItem,
-    //   }}
-    // >
-    //same can be written as below
-
     <TodoItemsFromContext.Provider
       value={{
         todoitems: itemList,
